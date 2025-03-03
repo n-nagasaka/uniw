fb7098cf940673c16fb3202aca8cd07f019c51e2
n-nagasaka <n-nagasaka@uniteraty.com>
2025-03-03 16:14:46 +0900

    Change Async2/Worker.cs

--- study/Async2/Worker.cs
+++ study/Async2/Worker.cs
@@ -4,9 +4,20 @@ public class Worker : BackgroundService
 {
     private readonly ILogger<Worker> _logger;
 
-    public Worker(ILogger<Worker> logger)
+    private readonly IHostApplicationLifetime _appLifetime;
+
+    private Worker(ILogger<Worker> logger, IHostApplicationLifetime appLifetime)
     {
         _logger = logger;
+        _appLifetime = appLifetime;
+    }
+
+    public override Task StartAsync(CancellationToken cancellationToken)
+    {
+        _appLifetime.ApplicationStarted.Register(() => Task.Run(Exec1, cancellationToken));
+        _appLifetime.ApplicationStarted.Register(Exec2);
+        _logger.LogInformation("StartAsync");
+        return Task.CompletedTask;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -19,5 +30,19 @@ public class Worker : BackgroundService
             }
             await Task.Delay(1000, stoppingToken);
         }
+    }
+
+    protected async Task Exec1()
+    {
+        _logger.LogInformation("Exec1: hello");
+        await Task.Delay(1000);
+        _logger.LogInformation("Exec1: bye");
+    }
+
+    protected async void Exec2()
+    {
+        _logger.LogInformation("Exec2: hello");
+        await Task.Delay(1000);
+        _logger.LogInformation("Exec2: bye");
     }
 }

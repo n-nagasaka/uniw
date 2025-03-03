46741e8fce1c6db078310641169ed8881447dcad
n-nagasaka <n-nagasaka@uniteraty.com>
2025-03-03 14:48:58 +0900

    Add AsyncVoid and AysncTask

--- study/Async1/Async1/Program.cs
+++ study/Async1/Async1/Program.cs
@@ -1,2 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
 Console.WriteLine("Hello, World!");
+var main = new Main();
+
+Console.WriteLine("Call AsyncTask");
+await main.AsyncTask();
+Console.WriteLine("AsyncTask returned");
+
+// Console.WriteLine("Call AsyncVoid");
+// error CS4008: void' を待機することができません
+// await main.AsyncVoid();
+// Console.WriteLine("AsyncVoid returned");
+
+class Main
+{
+    public async Task AsyncTask()
+    {
+        Console.WriteLine("AsyncTask started");
+        await Task.Delay(1000);  // Wait one second
+        Console.WriteLine("AsyncTask completed");
+    }
+
+    public async void AsyncVoid() 
+    {
+        Console.WriteLine("AsyncVoid started");
+        await Task.Delay(1000);
+        Console.WriteLine("AsyncVoic completed");
+    }
+}
--- study/Async1/README.md
+++ study/Async1/README.md
@@ -1,3 +1,12 @@
 # Async1
 
 async void / async Task の調査
+
+
+警告
+![warning screenshot](./images/err.png)
+Avoid using 'async' for method with the 'void' return type: any exceptions unhandled by the method might lead to the process crash
+
+async'を'void'戻り値のメソッドに使用するのは避けること。
+メソッドで処理されなかった例外は、プロセスのクラッシュにつながる可能性があります。
+

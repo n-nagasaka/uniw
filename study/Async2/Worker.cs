f800a421d3c1fd136c9ab4df8981d6a513ed176a
n-nagasaka <n-nagasaka@uniteraty.com>
2025-03-04 10:20:08 +0900

    feat: Enhance Worker with error handling and async void registration

--- study/Async2/Worker.cs
+++ study/Async2/Worker.cs
@@ -6,16 +6,25 @@ public class Worker : BackgroundService
 
     private readonly IHostApplicationLifetime _appLifetime;
 
+    private readonly bool _asyncVoid;  // async void メンバーを登録
+    private readonly bool _error;  // error を発生させる
+
     public Worker(ILogger<Worker> logger, IHostApplicationLifetime appLifetime)
     {
         _logger = logger;
         _appLifetime = appLifetime;
+        var args = Environment.GetCommandLineArgs();
+
+        _asyncVoid = args.Contains("void");
+        _error = args.Contains("error");
     }
 
     public override Task StartAsync(CancellationToken cancellationToken)
     {
         _appLifetime.ApplicationStarted.Register(() => Task.Run(Exec1, cancellationToken));
-        _appLifetime.ApplicationStarted.Register(Exec2);
+        if (_asyncVoid) {
+            _appLifetime.ApplicationStarted.Register(Exec2);
+        }
         _logger.LogInformation("StartAsync");
         return Task.CompletedTask;
     }
@@ -32,17 +41,40 @@ public class Worker : BackgroundService
         }
     }
 
+
     protected async Task Exec1()
     {
-        _logger.LogInformation("Exec1: hello");
-        await Task.Delay(1000);
-        _logger.LogInformation("Exec1: bye");
+        try {
+
+            _logger.LogInformation("Exec1: hello");
+            await Task.Delay(1000);
+            if (_error) {
+                throw new Exception("Exec1: error");
+            }
+            _logger.LogInformation("Exec1: bye");
+
+
+        } catch (Exception ex) {
+            _logger.LogError(ex.ToString());
+            throw;
+        }
     }
 
     protected async void Exec2()
     {
-        _logger.LogInformation("Exec2: hello");
-        await Task.Delay(1000);
-        _logger.LogInformation("Exec2: bye");
+        try {
+
+            _logger.LogInformation("Exec2: hello");
+            await Task.Delay(1000);
+            if (_error) {
+                throw new Exception("Exec2: error");
+            }
+            _logger.LogInformation("Exec2: bye");
+
+
+        } catch (Exception ex) {
+            _logger.LogError(ex.ToString());
+            throw;
+        }
     }
 }

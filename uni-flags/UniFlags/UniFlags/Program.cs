c94ecaee19f7d3e70aec378f537c1876ab448306
n-nagasaka <n-nagasaka@uniteraty.com>
2025-03-27 22:37:05 +0900

    feat: Add Program.cs

--- uni-flags/UniFlags/UniFlags/Program.cs
+++ uni-flags/UniFlags/UniFlags/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+class Flag
+{
+    public string ShortName { get; set; }
+
+    public string LongName { get; set; }
+
+    public string Name { get; }
+
+    public string Description { get; set; }
+}
+
+class UniFlag
+{
+    public List<Flag> Flags { get; set; }
+
+    public string Usage()
+    {
+        return "Usage: uniflag ";
+    }
+}

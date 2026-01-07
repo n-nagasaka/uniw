6b4b4bcd8d8aab49f6f505a56d6d9f8084cbc7bd
n-nagasaka <n-nagasaka@uniteraty.com>
2026-01-07 23:51:04 +0900

    feat: Enhance layout with Bootstrap styling for calendar link

--- hello.pl
+++ hello.pl
@@ -56,11 +56,17 @@ __DATA__
 % layout 'default';
 % title 'Welcome';
 <h1>Welcome to the Mojolicious real-time web framework!</h1>
-<p><a href="/calendar">カレンダーを見る</a></p>
+<p><a class="btn btn-primary" href="/calendar">カレンダーを見る</a></p>
 
 @@ layouts/default.html.ep
 <!DOCTYPE html>
 <html>
-  <head><title><%= title %></title></head>
-  <body><%= content %></body>
+  <head>
+    <title><%= title %></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+  </head>
+  <body>
+    <%= content %>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+  </body>
 </html>
